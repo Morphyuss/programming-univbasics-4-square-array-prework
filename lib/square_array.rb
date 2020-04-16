50dbc2bff3b6d134527114ad054b6efe99322126
James Stamps Jr <jstampshere@gmail.com>
2020-04-16 22:12:17 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -3,8 +3,8 @@ def square_array(array)
   counter = 0
 
   while counter < array.length do
-    puts array[counter*counter]
-    counter += 1
+    puts array[counter]
+    counter += 1*1
   end
   array
 end
